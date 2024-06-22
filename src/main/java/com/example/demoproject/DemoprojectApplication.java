072ebc537339496fdd40b04988727b396e557c35
Ashish <ashishdawkhar123@gmail.com>
2024-06-22 18:27:27 +0530

    first commit

---
+++ src/main/java/com/example/demoproject/DemoprojectApplication.java
@@ -0,0 +1,16 @@
+package com.example.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class DemoprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoprojectApplication.class, args);
+    }
+
+
+}
